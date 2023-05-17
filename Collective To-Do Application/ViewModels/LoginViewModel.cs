bd833927af9ae7a624e678f8ec7c49107c18a08c
Lias Tif <ltif427@gmail.com>
2023-05-17 10:31:54 +0300

    Added LoginView & ViewModel

---
+++ Collective To-Do Application/ViewModels/LoginViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Collective_To_Do_Application.API;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Collective_To_Do_Application.ViewModels
+{
+    public class LoginViewModel : ObservableObject
+    {
+        public LoginViewModel(NavigationStore navigationStore)
+        {
+        }
+    }
+}
