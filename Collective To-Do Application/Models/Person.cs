da1f44b57d596186829736a44f2aa1239be77eb8
Lias Tif <ltif427@gmail.com>
2023-05-31 11:48:40 +0300

    Added Task & Person models

---
+++ Collective To-Do Application/Models/Person.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TeamScheduleApp.Models
+{
+    /// <summary>
+    /// A class that includes data about the user
+    /// </summary>
+    public class Person
+    {
+        [Required]
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string FName { get; set; }
+        [Required]
+        public string LName { get; set; }
+        [Required]
+        public string PinCode { get; set; }
+    }
+}
---
+++ Collective To-Do Application/Models/Task.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+using TeamScheduleApp.Models;
+
+namespace Collective_To_Do_Application.Models
+{
+    public class Task
+    {
+        [Required]
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Type { get; set; }
+        public Person? NextPerformer { get; set; }
+        public DateTime DeadLine { get; set; }
+        public bool IsDone { get; set; }
+        public string? Description { get; set; }
+    }
+}
