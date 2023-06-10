504ecc25bc5689ce56a21f8f6464c8056f883c0c
Lias Tif <ltif427@gmail.com>
2023-06-10 22:46:09 +0300

    Style sketch for ColumnHeader

---
+++ Collective To-Do Application/Conventers/SetMinWidthByDefaultWidthConventer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace Collective_To_Do_Application.Conventers
+{
+    class SetMinWidthByDefaultWidthConventer : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is TextBlock textBlock)
+                return textBlock.Width;
+            else
+                return 0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
